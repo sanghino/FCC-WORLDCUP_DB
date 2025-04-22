e375d0689fc97f490dc2bd57a9a5919f0b8445e3
Francesco <francesco.mori@gmail.com>
2025-04-22 22:47:26 +0200

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -3,10 +3,8 @@
 if [[ $1 == "test" ]]
 then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
-  echo "test"
 else
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
-  echo "prod"
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
@@ -20,14 +18,14 @@ do
     # if not found
     if [[ -z $WINNER_ID ]]
     then
-      $($PSQL "INSERT INTO teams (name) values ('$WINNER')");
+      echo $($PSQL "INSERT INTO teams (name) values ('$WINNER')");
       WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER'");
     fi
     if [[ -z $OPPONENT_ID ]]
     then
-      $($PSQL "INSERT INTO teams (name) values ('$OPPONENT')");
+      echo $($PSQL "INSERT INTO teams (name) values ('$OPPONENT')");
       OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT'");
     fi
-    $($PSQL "INSERT INTO games (winner_id, opponent_id, winner_goals, opponent_goals, year, round) values ($WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS, $YEAR, '$ROUND')");
+    echo $($PSQL "INSERT INTO games (winner_id, opponent_id, winner_goals, opponent_goals, year, round) values ($WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS, $YEAR, '$ROUND')");
   fi
 done
